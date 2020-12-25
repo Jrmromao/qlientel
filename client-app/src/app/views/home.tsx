32622a6cde8186162cab92bccf7f0ad329bac636
Joao Romao <jfromao@masterlink.ie>
2020-12-25 12:43:28 +0000

    test commit: rm default values from login form

--- client-app/src/app/views/home.tsx
+++ client-app/src/app/views/home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
     <Segment inverted textAlign="center" vertical className="masthead">
       <Container text>
         <Header as="h1" inverted>
-          Qlientel
+          Qlientel test Azure
         </Header>
 
         {isLoggedIn && user && token ? (
