c3b73b3e0237ab3e80c5d22e3e54926614d6f020
Joao Romao <jfromao@masterlink.ie>
2020-12-25 21:41:10 +0000

    stahe wwwroot

---
+++ API/wwwroot/precache-manifest.0894a6a4ccf10b68cb105b436c56bde5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5730480f8902c1c3c5adc29aac1c35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a9e9979cb2aa9bb364",
+    "url": "/static/css/2.2aa214d5.chunk.css"
+  },
+  {
+    "revision": "738d7657954644f53bd9",
+    "url": "/static/css/main.5268c108.chunk.css"
+  },
+  {
+    "revision": "d3a9e9979cb2aa9bb364",
+    "url": "/static/js/2.c4c19a3a.chunk.js"
+  },
+  {
+    "revision": "a97142ef21ff72a1cdac64c26781b0f1",
+    "url": "/static/js/2.c4c19a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "738d7657954644f53bd9",
+    "url": "/static/js/main.647aafd6.chunk.js"
+  },
+  {
+    "revision": "02d9227e77ec1abd79db",
+    "url": "/static/js/runtime-main.b47abeb5.js"
+  },
+  {
+    "revision": "1534633cb2e2b2ecfaf2d0a13d31f62b",
+    "url": "/static/media/g1.1534633c.png"
+  },
+  {
+    "revision": "696fa3222b4f8add7c19bd10f7b2649f",
+    "url": "/static/media/g2.696fa322.png"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "6127d7810a9688726ce455216b52a1e9",
+    "url": "/static/media/map.6127d781.png"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
